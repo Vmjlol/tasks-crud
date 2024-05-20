9e13d4ce46ef74b3d7415cbc9934cdb5daaac2ea
Vmjlol <maniezzojr@yahoo.com.br>
2024-05-20 07:28:20 -0300

    upt

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('task')->controller(TaskController::class)->group(function() {
     Route::post('', 'create');
     Route::get('', 'index');
+    Route::get('/today', 'today');
+    Route::get('/late', 'late');
     Route::get('/{task}', 'show');
     Route::put('/{task}', 'update');
     Route::patch('/{task}', 'updateStatus');
